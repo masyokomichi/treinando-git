3d826e86e50fc193e3f176496dbecf03b473bf30
masyokomichi <49153488+masyokomichi@users.noreply.github.com>
2021-10-09 15:21:12 -0300

    alterações de teste professor

--- exec.py
+++ exec.py
@@ -1,4 +1,10 @@
-a = 1
-if(a==1){
-    print("Olá Mundo")
-}
+#a = 1
+#if(a==1){
+#    print("Olá Mundo")
+#}
+
+#professor mandou colocar essas alterações
+
+print("Digite o seu nome:")
+nome = input()
+print("O nome digitado foi: " + nome)
